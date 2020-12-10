02cdf5cd6988f6c9cf69c50dfe82633ca3a2b1fb
Laura Cole <lauracole1900@comcast.net>
2020-12-10 00:37:57 -0700

    file structure

---
+++ server.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const path = require("path");
+const PORT = process.env.PORT || 3001;
+const routes = require("./api_routes")
+const app = express();
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Serve up static assets (usually on heroku)
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+
+
+// Define API routes here
+app.use(routes);
+// Define Express routes first
+// React hits the routes, not Mongo
+
+// All other requests sent to the React app
+// Define any API routes before this runs
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "./client/build/index.html"));
+});
+
+mongoose.connect(
+    process.env.MONGODB_URI || 'mongodb://localhost/conference',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    }
+  );
+
+app.listen(PORT, () => {
+  console.log(`🌎 ==> API server now on port ${PORT}`);
+});
---
+++ server/models/index.js
@@ -0,0 +1,8 @@
+module.exports = {
+  Attendee: require("./attendeeModel"),
+  Conference: require("./conferenceModel"),
+  Exhibitor: require("./exhibitorModel"),
+  Presenter: require("./presenterModel"),
+  Session: require("./sessionModel"),
+  User: require("./userModel")
+}
