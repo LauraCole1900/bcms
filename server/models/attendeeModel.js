1e2dacabcdd26efb2982ebdd66585a7be7d18316
Laura Cole <lauracole1900@comcast.net>
2020-12-10 13:23:59 -0700

    added confid to attendeemodel

--- server/models/attendeeModel.js
+++ server/models/attendeeModel.js
@@ -2,6 +2,10 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const attendeeSchema = new Schema({
+  confId: {
+    type: String,
+    required: true
+  },
   email: {
     type: String,
     match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
