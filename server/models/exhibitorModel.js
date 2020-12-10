55f8a6a87045b2605b1b5363d7b0483d96b8eb39
Laura Cole <lauracole1900@comcast.net>
2020-12-10 15:37:26 -0700

    drafted exhibitorModel

---
+++ server/models/exhibitorModel.js
@@ -0,0 +1,45 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const exhibitorSchema = new Schema({
+  confId: {
+    type: String,
+    required: true
+  },
+  exhEmail: {
+    type: String,
+    match: [/.+@.+\..+/, "Please enter a valid e-mail address"],
+    required: true
+  },
+  exhCompany: {
+    type: String,
+    required: true
+  },
+  exhWorkers: {
+    type: Number,
+    required: true
+  },
+  exhNames: {
+    type: [ String ],
+    required: true
+  },
+  exhPhone: {
+    type: String,
+    required: true
+  },
+  exhSpaces: {
+    type: String,
+    required: true
+  },
+  exhAttend: {
+    type: Boolean,
+    required: true
+  },
+  exhPaid: {
+    type: Boolean
+  }
+})
+
+const Exhibitor = mongoose.model("Exhibitor", exhibitorSchema);
+
+module.exports = Exhibitor
