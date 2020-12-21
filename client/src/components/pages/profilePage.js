bfd655f832800df1f533021a51c553c6697b14d9
Laura Cole <lauracole1900@comcast.net>
2020-12-21 03:01:18 -0700

    added new conf button to profile page

--- client/src/components/pages/profilePage.js
+++ client/src/components/pages/profilePage.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Link, useHistory } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Container, Row, Col, Button, Card, Image, ButtonGroup, ToggleButton } from "react-bootstrap";
 import Conference from "../conferenceCard";
@@ -9,6 +9,7 @@ import "./style.css";
 const Profile = () => {
   const { user, isAuthenticated } = useAuth0();
   const [whichConf, setWhichConf] = useState();
+  const location = useLocation();
 
 
   const handleInputChange = (e) => {
@@ -36,24 +37,35 @@ const Profile = () => {
           </Card>
           <Col sm={4}></Col>
         </Row>
-        <div className="myConfs">
-          <h1>My Conferences</h1>
-        </div>
-        <ButtonGroup toggle>
-          <ToggleButton type="radio" name="whichConf" value={1} checked={whichConf === 1} onChange={handleInputChange}>
-            Created
-          </ToggleButton>
-          <ToggleButton type="radio" name="whichConf" value={2} checked={whichConf === 2} onChange={handleInputChange}>
-            Attending
-          </ToggleButton>
-          <ToggleButton type="radio" name="whichConf" value={3} checked={whichConf === 3} onChange={handleInputChange}>
-            Presenting
-          </ToggleButton>
-          <ToggleButton type="radio" name="whichConf" value={4} checked={whichConf === 4} onChange={handleInputChange}>
-            Exhibiting
-          </ToggleButton>
-        </ButtonGroup>
-        
+        <Row>
+          <div className="myConfs">
+            <h1>My Conferences</h1>
+          </div>
+        </Row>
+        <Row>
+          <Col sm={6}>
+            <ButtonGroup toggle>
+              <ToggleButton type="radio" name="whichConf" value={1} checked={whichConf === 1} onChange={handleInputChange}>
+                Created
+              </ToggleButton>
+              <ToggleButton type="radio" name="whichConf" value={2} checked={whichConf === 2} onChange={handleInputChange}>
+                Attending
+              </ToggleButton>
+              <ToggleButton type="radio" name="whichConf" value={3} checked={whichConf === 3} onChange={handleInputChange}>
+                Presenting
+              </ToggleButton>
+              <ToggleButton type="radio" name="whichConf" value={4} checked={whichConf === 4} onChange={handleInputChange}>
+                Exhibiting
+              </ToggleButton>
+            </ButtonGroup>
+          </Col>
+          <Col sm={4}></Col>
+          <Col sm={2}>
+            <Link to="/new_conference" className={location.pathname === "/create_conference" ? "link active" : "link"}>
+              <Button data-toggle="popover" title="Create a new conference">New Conference</Button>
+            </Link>
+          </Col>
+        </Row>
       </Container >
     )
   )
