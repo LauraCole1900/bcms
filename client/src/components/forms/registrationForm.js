b50e82cceadf9607ba21f68ce9d5312c6827ddc8
Laura Cole <lauracole1900@comcast.net>
2020-12-31 05:34:46 -0700

    continuing work on registrationForm

--- client/src/components/forms/registrationForm.js
+++ client/src/components/forms/registrationForm.js
@@ -12,7 +12,7 @@ const Registration = () => {
 
   const urlArray = window.location.href.split("/")
   const confId = urlArray[urlArray.length - 1]
-  
+
   useEffect(() => {
     setAttendee({ ...attendee, confId: confId })
     setPageReady(true);
@@ -21,6 +21,33 @@ const Registration = () => {
   const handleInputChange = (e) => {
     setAttendee({ ...attendee, [e.target.name]: e.target.value })
   };
+
+  const handleFormUpdate = (e) => {
+    e.preventDefault();
+    console.log("Attendee update", confId, user.email);
+    AttendeeAPI.updateAttendee({ ...attendee }, confId, user.email)
+      .then(history.push("/attendee_updated"))
+      .catch(err => console.log(err))
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log("Attendee submit")
+    AttendeeAPI.registerAttendee({ ...attendee, email: user.email })
+      .then(history.push(`/register_success/${confId}`))
+      .catch(err => console.log(err));
+  }
+
+  return (
+    <>
+      { pageReady === true &&
+        isAuthenticated && (
+          <Container>
+
+          </Container>
+        )}
+    </>
+  )
 
 }
 
