2faad4bad354ca279796db85291504cef5ae8351
Laura Cole <lauracole1900@comcast.net>
2021-01-02 00:24:45 -0700

    consolidated routing in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -21,23 +21,17 @@ function App() {
       </header>
       <main>
         <Route exact path="/login" component={Login} />
-        {/* <Route exact path="/contact_bcms" component={ContactBCMS} /> */}
+        <Route exact path="/contact_bcms" component={ContactBCMS} />
         <Route exact path="/profile" component={Profile} />
-        {/*   <Route path="/conferences/*" component={ConfDetails} />
-              <Route path="/conferences/schedule/*" component={Schedule} />
-              <Route path="/conferences/venue/*" component={Venue} /> */}
-        <Route path="/new_conference" component={ConferenceForm} />
-        <Route path="/edit_conference/*" component={ConferenceForm} />
-        {/*   <Route path="/add_session/*" component={SessionForm} />
-              <Route path="/propose_session/*" component={SessionProposal} /> */}
+        <Route path="/conferences/*" component={ConfDetails} />
+        <Route path="/conferences/schedule/*" component={Schedule} />
+        <Route path="/conferences/venue/*" component={Venue} />
+        <Route path={["/new_conference", "/edit_conference/*"]} component={ConferenceForm} />
+        <Route path={["/add_session/*", "/edit_session/*"]} component={SessionForm} />
+        <Route path="/propose_session/*" component={SessionProposal} />
         <Route path="/register_exhibit/*" component={ExhibitForm} />
         <Route path="/update_user" component={UpdateUser} />
-        <Route path="/conference_created" component={Success} />
-        <Route path="/session_added" component={Success} />
-        <Route path="/deleted" component={Success} />
-        <Route path="/conference_updated" component={Success} />
-        <Route path="/session_updated" component={Success} />
-        <Route path="/user_updated" component={Success} />
+        <Route path={["/conference_created", "/conference_updated", "/deleted", "/session_added", "/session_updated", "user_updated"]} component={Success} />
         <Route path="/register_attend/*" component={Registration} />
         <Route exact path={["/", "/conferences"]} component={AllConfs} />
       </main>
