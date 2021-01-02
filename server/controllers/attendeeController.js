a74f690ffc2dfb1bd4e10ce620af9d5deb70d9da
Laura Cole <lauracole1900@comcast.net>
2021-01-01 23:57:36 -0700

    added findByEmail to attendeeController, not convinced it's correct

--- server/controllers/attendeeController.js
+++ server/controllers/attendeeController.js
@@ -5,58 +5,66 @@ module.exports = {
   create: function (req, res) {
     console.log("from attendeeController create", req.params.email)
     db.Attendee
-    .findOne({ email: req.params.email }, function (err, attendee) {
-      if (err) {
-        let err = new Error("We're sorry, it looks like gremlins have gotten into our database. Please try again.")
-        err.status = 400;
-        return err;
-      } else if (attendee) {
-        let err = new Error("We're sorry, that email has already been registered. Please use another email.")
-        err.status = 400;
-        return err;
-      } else {
-        db.Attendee
-        .create(req.body)
-        .then(dbModel => res.json(dbModel))
-        .catch(err => res.status(422).json(err))
-      }
-    })
+      .findOne({ email: req.params.email }, function (err, attendee) {
+        if (err) {
+          let err = new Error("We're sorry, it looks like gremlins have gotten into our database. Please try again.")
+          err.status = 400;
+          return err;
+        } else if (attendee) {
+          let err = new Error("We're sorry, that email has already been registered. Please use another email.")
+          err.status = 400;
+          return err;
+        } else {
+          db.Attendee
+            .create(req.body)
+            .then(dbModel => res.json(dbModel))
+            .catch(err => res.status(422).json(err))
+        }
+      })
   },
 
 
   // FIND all attendees
   findAll: function (req, res) {
     db.Attendee
-    .find({})
-    .sort({ date: -1 })
-    .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err))
+      .find({})
+      .sort({ date: -1 })
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
   },
 
   // FIND attendee by ID
   findById: function (req, res) {
     db.Attendee
-    .findById(req.params.id)
-    .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err))
+      .findById(req.params.id)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
+  },
+
+  // FIND attendee by email
+  findByEmail: function (req, res) {
+    db.Attendee
+      .findById(req.body.email)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
   },
 
 
   // UPDATE attendee
   updateAttendee: function (req, res) {
     db.Attendee
-    .findOneAndUpdate({ _id: req.params.id }, req.body)
-    .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err))
+      .findOneAndUpdate({ _id: req.params.id }, req.body)
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
   },
 
 
   // DELETE attendee
   removeAttendee: function (req, res) {
     db.Attendee
-    .findById({ _id: req.params.id })
-    .then(dbModel => dbModel.remove())
-    .then(dbModel => res.json(dbModel))
-    .catch(err => res.status(422).json(err))
+      .findById({ _id: req.params.id })
+      .then(dbModel => dbModel.remove())
+      .then(dbModel => res.json(dbModel))
+      .catch(err => res.status(422).json(err))
   }
 }
