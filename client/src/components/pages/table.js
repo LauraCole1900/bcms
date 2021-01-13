e4bade5f513323a3b127b68e214b8f1c32790a24
Laura Cole <lauracole1900@comcast.net>
2021-01-13 04:24:37 -0700

    started setting const in table.js

--- client/src/components/pages/table.js
+++ client/src/components/pages/table.js
@@ -1,7 +1,9 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
+import { ConferenceCard, UserCard } from "../cards";
 import { AttendeeAPI, ExhibitorAPI, PresenterAPI } from "../../utils/api";
 import "./style.css";
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Table = (e) => {
   // pull confId out of URL
@@ -14,7 +16,15 @@ const Table = (e) => {
   // user card
   // conference card
 
+  const { user, isAuthenticated } = useAuth0();
+  const location = useLocation();
+  const [attendees, setAttendees] = useState([]);
+  const [exhibitors, setExhibitors] = useState([]);
+  const [presenters, setPresenters] = useState([]);
 
+  const urlArray = window.location.href.split("/");
+  const confId = urlArray[urlArray.length - 1];
+  const dataSet = urlArray[urlArray.length - 2];
 }
 
 export default Table;
