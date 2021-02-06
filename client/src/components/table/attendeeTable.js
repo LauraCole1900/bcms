27447e26b21c66bf7903a455a73439cc1aa7a455
Laura Cole <lauracole1900@comcast.net>
2021-02-06 15:30:40 -0700

    attendeeTable handleInputChange(), needs PUT route

--- client/src/components/table/attendeeTable.js
+++ client/src/components/table/attendeeTable.js
@@ -1,8 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import "./style.css";
 
 const AttendeeTable = ({ attendees }) => {
+  const [attendee, setAttendee] = useState();
+
+  const handleInputChange = (e) => {
+    switch (e.isAdmin) {
+      case true:
+        setAttendee({ ...attendee, [e.isAdmin]: "no" })
+        break;
+      default:
+        setAttendee({ ...attendee, [e.isAdmin]: "yes" })
+    }
+  }
+
   return (
     <>
       {attendees.map(e => (
@@ -15,7 +27,7 @@ const AttendeeTable = ({ attendees }) => {
           <td>{e.emergencyContactName}</td>
           <td>{e.emergencyContactPhone}</td>
           <td>{e.allergies}</td>
-          <td><Form.Check type="checkbox" checked={e.isAdmin === true} /></td>
+          <td><Form.Check type="checkbox" name="isAdmin" value="yes" checked={e.isAdmin === true} onChange={handleInputChange} /></td>
         </tr>
       ))
       }
