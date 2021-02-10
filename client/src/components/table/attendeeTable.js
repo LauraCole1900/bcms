6a91f77d9b94b0b86e43dec323bb94d091241017
Laura Cole <lauracole1900@comcast.net>
2021-02-09 18:42:03 -0700

    single email inputs into conference.confAdmins, need to fix so keeps array

--- client/src/components/table/attendeeTable.js
+++ client/src/components/table/attendeeTable.js
@@ -1,12 +1,21 @@
 import React from "react";
 import { Form } from "react-bootstrap";
-import { AttendeeAPI } from "../../utils/api";
+import { AttendeeAPI, ConferenceAPI } from "../../utils/api";
 import "./style.css";
 
 const AttendeeTable = (props) => {
+  // Map through props.attendees array
+  // Where props.attendee._id === e.target.dataset.id
+  // Get props.attendee.email
+  // Update Conference document with props.attendee.email in the confAdmins array
+  const getEmail = (id) => {
+    const adminObj = props.attendees.find(attendees => attendees._id === id)
+    const adminEmail = adminObj.email
+    return adminEmail
+  }
 
   // Click handler for checkbox
-  const handleInputChange = (e) => {
+  const handleInputChange = async (e) => {
     console.log("Attendee table", e.target.value, e.target.dataset.id);
     let adminConf;
     // Define data to be changed based on existing checkbox value
@@ -17,10 +26,23 @@ const AttendeeTable = (props) => {
       default:
         adminConf = true;
     }
-    // API call to update database document
+    // API call to update attendee document
     AttendeeAPI.updateAttendeeById(e.target.dataset.id, { [e.target.name]: adminConf })
     .then(props.callback(props.confId))
       .catch(err => console.log(err))
+    let adminEmail = await getEmail(e.target.dataset.id)
+    switch (adminConf) {
+      case true:
+        console.log({adminEmail}, props.confId)
+        ConferenceAPI.updateConference({ confAdmins: adminEmail }, props.confId)
+        .catch(err => console.log(err))
+        break;
+      default:
+        // GET conference details?
+        // iterate through confAdmins to find adminEmail
+        // delete adminEmail from confAdmins
+    }
+
   }
 
   return (
