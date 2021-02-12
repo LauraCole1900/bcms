89bfe4a1b08fd716b403bcfe0ec6595a80004b2d
Laura Cole <lauracole1900@comcast.net>
2021-02-12 14:06:29 -0700

    renamed e variable to conf in confCard.js

--- client/src/components/cards/conferenceCard.js
+++ client/src/components/cards/conferenceCard.js
@@ -36,18 +36,18 @@ function Conference({ conference }) {
   return (
     <>
       {(cardRender === true) &&
-        conference.map(e => (
-          <Card className="confCard" key={e._id}>
+        conference.map(conf => (
+          <Card className="confCard" key={conf._id}>
             <Card.Header className="confTitle">
               <Row>
                 <Col sm={11}>
-                  <h2>{e.confName}</h2>
-                  <p className="org">Presented by {e.confOrg}</p>
+                  <h2>{conf.confName}</h2>
+                  <p className="org">Presented by {conf.confOrg}</p>
                 </Col>
                 <Col sm={1}>
                   {isAuthenticated &&
-                    (user.email === e.creatorEmail) &&
-                    <Button data-toggle="popover" title="Delete this conference" className="deletebtn" onClick={() => handleDelete(e._id)}>
+                    (user.email === conf.creatorEmail) &&
+                    <Button data-toggle="popover" title="Delete this conference" className="deletebtn" onClick={() => handleDelete(conf._id)}>
                       <Image fluid src="/images/trash-can.png" className="delete" alt="Delete" />
                     </Button>}
                 </Col>
@@ -56,38 +56,38 @@ function Conference({ conference }) {
             <Card.Body className="cardBody">
               <Row>
                 <Col sm={8}>
-                  {(e.confWaiver === "yes") &&
+                  {(conf.confWaiver === "yes") &&
                     <div className="alert">
                       <h5>A signed liability waiver will be required to participate in this event. It will be available at check-in to the event.</h5>
                     </div>}
-                  <Card.Text>{e.confDesc}</Card.Text>
+                  <Card.Text>{conf.confDesc}</Card.Text>
                 </Col>
                 <Col sm={4} className="vitals">
-                  {e.numDays === 1
-                  ? <Row><p>Date: <Moment format="ddd, D MMM YYYY" withTitle>{e.startDate}</Moment></p></Row>
-                  : <Row><p>Dates: <Moment format="ddd, D MMM YYYY" withTitle>{e.startDate}</Moment> - <Moment format="ddd, D MMM YYYY" withTitle>{e.endDate}</Moment></p></Row>}
-                  <Row><p>Times: {e.confStartTime} - {e.confEndTime}</p></Row>
-                  <Row><p>Type: {e.confType}</p></Row>
+                  {conf.numDays === 1
+                    ? <div><Row><p>When: <Moment format="ddd, D MMM YYYY" withTitle>{conf.startDate}</Moment> @{conf.confStartTime} - {conf.confEndTime}</p></Row></div>
+                    : <div><Row><p>When: <Moment format="ddd, D MMM YYYY" withTitle>{conf.startDate}</Moment> @{conf.confStartTime} - <Moment format="ddd, D MMM YYYY" withTitle>{conf.endDate}</Moment> @{conf.confEndTime}</p></Row></div>}
+                  {/* <Row><p>Times: {conf.confStartTime} - {conf.confEndTime}</p></Row> */}
+                  <Row><p>Type: {conf.confType}</p></Row>
                   <Row>
-                    {(e.confType === "Live") &&
-                      <p><a href={`https://www.google.com/maps/search/${e.confLoc.replace(" ", "+")}`} rel="noreferrer noopener" target="_blank">{e.confLoc}</a></p>}
-                    {(e.confType === "Virtual") &&
-                      (e.confLocUrl !== undefined) &&
-                      <p><a href={e.confLocUrl} rel="noreferrer noopener" target="_blank">{e.confLoc}</a></p>}
-                    {(e.confType === "Virtual") &&
-                      (e.confLocUrl === undefined) &&
-                      <p>{e.confLoc}</p>}
+                    {(conf.confType === "Live") &&
+                      <p><a href={`https://www.google.com/maps/search/${conf.confLoc.replace(" ", "+")}`} rel="noreferrer noopener" target="_blank">{conf.confLoc}</a></p>}
+                    {(conf.confType === "Virtual") &&
+                      (conf.confLocUrl !== undefined) &&
+                      <p><a href={conf.confLocUrl} rel="noreferrer noopener" target="_blank">{conf.confLoc}</a></p>}
+                    {(conf.confType === "Virtual") &&
+                      (conf.confLocUrl === undefined) &&
+                      <p>{conf.confLoc}</p>}
                   </Row>
-                  {(e.confType === "Live") &&
-                    (e.confLocUrl !== undefined) &&
+                  {(conf.confType === "Live") &&
+                    (conf.confLocUrl !== undefined) &&
                     <Row>
-                      <p><a href={e.confLocUrl} rel="noreferrer noopener" target="_blank">{e.confLocName}</a></p>
+                      <p><a href={conf.confLocUrl} rel="noreferrer noopener" target="_blank">{conf.confLocName}</a></p>
                     </Row>}
                   <Row>
                     <Col sm={4}>
                       <Link to={{
                         state: { confInfo: conference },
-                        pathname: `/details/${e._id}`
+                        pathname: `/details/${conf._id}`
                       }}>
                         <Button data-toggle="popover" title="View conference details" className="button">View details</Button>
                       </Link>
@@ -97,13 +97,13 @@ function Conference({ conference }) {
               </Row>
               <Row>
                 {isAuthenticated &&
-                  (user.email === e.creatorEmail || e.confAdmins.includes(user.email)) &&
+                  (user.email === conf.creatorEmail || conf.confAdmins.includes(user.email)) &&
                   <div>
                     <Col sm={1}></Col>
                     <Col sm={1}>
                       <Link to={{
                         state: { confInfo: conference },
-                        pathname: `/attendees/${e._id}`
+                        pathname: `/attendees/${conf._id}`
                       }}>
                         <Button data-toggle="popover" title="View conference attendees" className="button">Attendees</Button>
                       </Link>
@@ -111,7 +111,7 @@ function Conference({ conference }) {
                     <Col sm={1}>
                       <Link to={{
                         state: { confInfo: conference },
-                        pathname: `/exhibitors/${e._id}`
+                        pathname: `/exhibitors/${conf._id}`
                       }}>
                         <Button data-toggle="popover" title="View conference exhibitors" className="button">Exhibitors</Button>
                       </Link>
@@ -119,7 +119,7 @@ function Conference({ conference }) {
                     <Col sm={1}>
                       <Link to={{
                         state: { confInfo: conference },
-                        pathname: `/presenters/${e._id}`
+                        pathname: `/presenters/${conf._id}`
                       }}>
                         <Button data-toggle="popover" title="View conference presenters" className="button">Presenters</Button>
                       </Link>
@@ -128,7 +128,7 @@ function Conference({ conference }) {
                     <Col sm={1}>
                       <Link to={{
                         state: { confInfo: conference },
-                        pathname: `/edit_conference/${e._id}`
+                        pathname: `/edit_conference/${conf._id}`
                       }}>
                         <Button data-toggle="popover" title="Edit this conference" className="button">Edit</Button>
                       </Link>
@@ -136,7 +136,7 @@ function Conference({ conference }) {
                     <Col sm={1}>
                       <Link to={{
                         state: { confInfo: conference },
-                        pathname: `/edit_schedule/${e._id}`
+                        pathname: `/edit_schedule/${conf._id}`
                       }}>
                         <Button data-toggle="popover" title="Edit conference schedule" className="button">Edit Schedule</Button>
                       </Link>
@@ -144,14 +144,14 @@ function Conference({ conference }) {
                   </div>}
 
                 {isAuthenticated &&
-                  user.email !== e.creatorEmail &&
-                  cardAttendConf.indexOf(e._id) >= 0 &&
+                  user.email !== conf.creatorEmail &&
+                  cardAttendConf.indexOf(conf._id) >= 0 &&
                   <div>
                     <Col sm={4}></Col>
                     <Col sm={1}>
                       <Link to={{
                         state: { confInfo: conference },
-                        pathname: `/unregister_confirm/${e._id}`
+                        pathname: `/unregister_confirm/${conf._id}`
                       }}>
                         <Button data-toggle="popover" title="Unregister from this conference" className="button">Unregister</Button>
                       </Link>
@@ -159,7 +159,7 @@ function Conference({ conference }) {
                     <Col sm={1}>
                       <Link to={{
                         state: { confInfo: conference },
-                        pathname: `/register_edit/${e._id}`
+                        pathname: `/register_edit/${conf._id}`
                       }}>
                         <Button data-toggle="popover" title="Edit your registration information" className="button">Edit registration</Button>
                       </Link>
@@ -167,14 +167,14 @@ function Conference({ conference }) {
                   </div>}
 
                 {isAuthenticated &&
-                  user.email !== e.creatorEmail &&
-                  cardAttendConf.indexOf(e._id) < 0 &&
+                  user.email !== conf.creatorEmail &&
+                  cardAttendConf.indexOf(conf._id) < 0 &&
                   <div>
                     <Col sm={4}></Col>
                     <Col sm={4}>
                       <Link to={{
                         state: { confInfo: conference },
-                        pathname: `/register_attend/${e._id}`
+                        pathname: `/register_attend/${conf._id}`
                       }}>
                         <Button data-toggle="popover" title="Register for this conference" className="button">Register</Button>
                       </Link>
