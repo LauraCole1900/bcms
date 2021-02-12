35acaea4eb5457e037f71c76ada10510018602b2
Laura Cole <lauracole1900@comcast.net>
2021-02-12 14:42:31 -0700

    renamed e variable to exh in exhibitorTable.js

--- client/src/components/table/exhibitorTable.js
+++ client/src/components/table/exhibitorTable.js
@@ -4,16 +4,16 @@ import "./style.css";
 const ExhibitorTable = ({ exhibitors }) => {
   return (
     <>
-      {exhibitors.map(e => (
-        <tr key={e._id}>
-          <td>{e.exhFamilyName}</td>
-          <td>{e.exhGivenName}</td>
-          <td>{e.exhEmail}</td>
-          <td>{e.exhPhone}</td>
-          <td>{e.exhCompany}</td>
-          <td>{e.exhWorkerNames}</td>
-          <td>{e.exhSpaces}</td>
-          <td>{e.exhAttend}</td>
+      {exhibitors.map(exh => (
+        <tr key={exh._id}>
+          <td>{exh.exhFamilyName}</td>
+          <td>{exh.exhGivenName}</td>
+          <td>{exh.exhEmail}</td>
+          <td>{exh.exhPhone}</td>
+          <td>{exh.exhCompany}</td>
+          <td>{exh.exhWorkerNames}</td>
+          <td>{exh.exhSpaces}</td>
+          <td>{exh.exhAttend}</td>
         </tr>
       ))
       }
