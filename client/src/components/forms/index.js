b7a4c0a1b5f20c0c9671ce9d6b40cdff383bbe24
Laura Cole <lauracole1900@comcast.net>
2021-02-13 19:58:20 -0700

    created scheduleForm.js

--- client/src/components/forms/index.js
+++ client/src/components/forms/index.js
@@ -2,6 +2,7 @@ export { default as ConferenceForm } from "./conferenceForm";
 export { default as Supplemental } from "./conferenceSupplemental";
 export { default as ExhibitForm } from "./exhibitForm";
 export { default as Registration } from "./registrationForm";
+export { default as ScheduleForm } from "./scheduleForm";
 export { default as SessionForm } from "./sessionForm";
 export { default as SessionProposal } from "./sessionProposal";
 export { default as UpdateUser } from "./userForm";
---
+++ client/src/components/forms/scheduleForm.js
@@ -0,0 +1,12 @@
+import React, { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+import { Container, Form, Row, Col, Button } from "react-bootstrap";
+import { useAuth0 } from "@auth0/auth0-react";
+import { ScheduleAPI, ConferenceAPI } from "../../utils/api";
+import "./style.css";
+
+const ScheduleForm = () => {
+
+};
+
+export default ScheduleForm;
