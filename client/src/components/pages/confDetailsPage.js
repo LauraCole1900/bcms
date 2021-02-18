8338706e9f917484e5f605a12c302467d53e82f8
Laura Cole <lauracole1900@comcast.net>
2021-02-17 17:46:41 -0700

    added some variables to confDetailsPage

--- client/src/components/pages/confDetailsPage.js
+++ client/src/components/pages/confDetailsPage.js
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import { Container, Row, Col } from "react-bootstrap";
 import { SessionAPI } from "../../utils/api";
 import "./style.css";
 
 const ConfDetails = () => {
+const [sessArray, setSessArray] = useState();
+const [searchBy, setSearchBy] = useState("allSess");
+const [search, setSearch] = useState("");
+const [pageReady, setPageReady] = useState(false);
+
+const urlArray = window.location.href.split("/")
+const confId = urlArray[urlArray.length - 1]
+
+
 
 }
 
