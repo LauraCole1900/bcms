da8f71eb79ba6dcf7b7b6eba24dad201c2ae9740
Laura Cole <lauracole1900@comcast.net>
2021-02-17 18:36:21 -0700

    added schedule, edit schedule and add session buttons to confDetailsPage

--- client/src/components/pages/confDetailsPage.js
+++ client/src/components/pages/confDetailsPage.js
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
-import { Container, Row, Col, Form, Card } from "react-bootstrap";
+import { Container, Row, Col, Form, Card, Button } from "react-bootstrap";
 import { ConferenceCard, SessionCard, UserCard } from "../cards"
-import { SessionAPI } from "../../utils/api";
+import { ConferenceAPI, SessionAPI } from "../../utils/api";
 import "./style.css";
 
 const ConfDetails = () => {
   const { user, isAuthenticated, loginWithRedirect } = useAuth0();
+  const [conference, setConference] = useState({})
   const [sessArray, setSessArray] = useState();
   const [searchBy, setSearchBy] = useState("allSess");
   const [search, setSearch] = useState("");
@@ -16,6 +18,12 @@ const ConfDetails = () => {
   const confId = urlArray[urlArray.length - 1]
 
   useEffect(() => {
+    ConferenceAPI.getConferenceById(confId)
+      .then(resp => {
+        setConference(resp.data)
+      })
+      .catch(err => console.log(err));
+
     SessionAPI.getSessions(confId)
       .then(resp => {
         const sessArr = resp.data;
@@ -46,28 +54,77 @@ const ConfDetails = () => {
               {isAuthenticated &&
                 <UserCard />}
             </Col>
-          </Row>
-          <Col sm={4}>
-            <Card.Body>
-              <Form inline>
-                <Row>
-                  <Form.Group controlId="sessSearchBy">
-                    <Form.Control as="select" name="searchBy" onChange={(e) => setSearchBy(e.target.value)}>
-                      <option value="allSess">View All Sessions</option>
-                      <option value="sessionName">Search by Session Name</option>
-                      <option value="presenter">Search by Presenter</option>
-                    </Form.Control>
-                  </Form.Group>
-                </Row>
-                {(searchBy !== "allSess") &&
+            <Col sm={4}>
+              <Card.Body>
+                <Form inline>
                   <Row>
-                    <div id="sessPageSearch">
-                      <Form.Control className="mr-lg-5 search-area" type="input" placeholder="Search sessions" value={search} onChange={(e) => setSearch(e.target.value)} />
-                    </div>
-                  </Row>}
-              </Form>
-            </Card.Body>
-          </Col>
+                    <Form.Group controlId="sessSearchBy">
+                      <Form.Control as="select" name="searchBy" onChange={(e) => setSearchBy(e.target.value)}>
+                        <option value="allSess">View All Sessions</option>
+                        <option value="sessionName">Search by Session Name</option>
+                        <option value="presenter">Search by Presenter</option>
+                      </Form.Control>
+                    </Form.Group>
+                  </Row>
+                  {(searchBy !== "allSess") &&
+                    <Row>
+                      <div id="sessPageSearch">
+                        <Form.Control className="mr-lg-5 search-area" type="input" placeholder="Search sessions" value={search} onChange={(e) => setSearch(e.target.value)} />
+                      </div>
+                    </Row>}
+                </Form>
+              </Card.Body>
+            </Col>
+          </Row>
+
+          {!isAuthenticated &&
+            <Row>
+              <h1 className="regRemind">Please <Link className="login" onClick={() => loginWithRedirect()}>
+                log in
+                </Link> to register.</h1>
+            </Row>}
+
+          <Row>
+            <Col sm={12}>
+              <ConferenceCard conference={conference} />
+            </Col>
+          </Row>
+
+          <Row>
+            <Col sm={1}></Col>
+            <Col sm={2}>
+              <Link to={{
+                state: {confInfo: conference},
+                pathname: `/schedule/${confId}`
+              }}>
+                <Button data-toggle="popover" title="View schedule" className="button">Schedule</Button>
+              </Link>
+            </Col>
+            <Col sm={1}></Col>
+            {isAuthenticated &&
+              (user.email === conference.creatorEmail || conference.confAdmins.includes(user.email)) &&
+              <div>
+                <Col sm={2}>
+                  <Link to={{
+                    state: {confInfo: conference},
+                    pathname: `/edit_schedule/${confId}`
+                  }}>
+                    <Button data-toggle="popover" title="Edit schedule" className="button">Edit Schedule</Button>
+                  </Link>
+                </Col>
+                <Col sm={1}></Col>
+                <Col sm={2}>
+                  <Link to={{
+                    state: { confInfo: conference },
+                    pathname: `/add_session/${confId}`
+                  }}>
+                    <Button data-toggle="popover" title="Add session" className="button">Add Session</Button>
+                  </Link>
+                </Col>
+              </div>}
+          </Row>
+
+          
         </Container>
       }
     </>
