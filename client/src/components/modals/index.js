63e629b6a1b74ee703839de7940ce219eefd6a9e
Laura Cole <lauracole1900@comcast.net>
2021-03-15 19:38:47 -0600

    deleted modal.js

--- client/src/components/modals/index.js
+++ client/src/components/modals/index.js
@@ -1,4 +1,3 @@
 export { default as ConfirmModal } from "./confirm";
 export { default as ErrorModal } from "./error";
-export { default as ModalIdx } from "./modal";
 export { default as SuccessModal } from "./success";
--- client/src/components/modals/modal.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { Modal } from "react-bootstrap";
-
-const ModalIdx = () => {
-
-}
-
-export default ModalIdx;
