e4ba948305171cc051a65bc8b8ccf2d1b82e26b8
Laura Cole <lauracole1900@comcast.net>
2021-04-14 20:38:23 -0600

    created form validation files

---
+++ client/src/components/validation/index.js
@@ -0,0 +1,5 @@
+export { default as ConfValidate } from "./conferenceFormValidationRules";
+export { default as ExhibitValidate} from "./exhibitFormValidationRules";
+export { default as RegValidate } from "./registrationFormValidationRules";
+export { default as SessValidate } from "./sessionFormValidationRules";
+export { default as SessPropValidate } from "./sessionProposalValidationRules";
