0dc2d8ec56f0a65264ddd332c66fe793cd404340
Laura Cole <lauracole1900@comcast.net>
2021-04-14 20:29:46 -0600

    refactored regForm to avoid uncontrolled-to-controlled-element error

--- client/src/components/forms/registrationForm.js
+++ client/src/components/forms/registrationForm.js
@@ -19,6 +19,8 @@ const Registration = () => {
     employerAddress: "",
     emergencyContactName: "",
     emergencyContactPhone: "",
+    allergyConfirm: "",
+    allergies: "",
   });
   const [errThrown, setErrThrown] = useState();
 
