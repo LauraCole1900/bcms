2f946a396be693db94acb96faff8e66bd6b4fca0
Laura Cole <lauracole1900@comcast.net>
2021-05-17 05:26:16 -0600

    worked on schedForm

--- client/src/components/forms/scheduleForm.js
+++ client/src/components/forms/scheduleForm.js
@@ -8,7 +8,9 @@ import "./style.css";
 const ScheduleForm = () => {
   const { user, isAuthenticated, loginWithRedirect } = useAuth0();
   const [conference, setConference] = useState();
+  const [schedule, setSchedule] = useState();
   const [confReady, setConfReady] = useState(false);
+
 
   // Grabs conference ID from URL
   const urlArray = window.location.href.split("/")
@@ -24,6 +26,19 @@ const ScheduleForm = () => {
         setConfReady(true)
       })
   }
+
+  // Handles input changes to form fields
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    if (name === "schedRooms") {
+      // Splits input to sessPresEmail field at commas to create an array
+      let rooms = value.split(",")
+      setSchedule({ ...schedule, schedRooms: rooms })
+    } else if (name === "schedTimes") {
+      let times = value.split(",")
+      setSchedule({ ...schedule, schedTimes: times })
+    }
+  };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
@@ -57,13 +72,41 @@ const ScheduleForm = () => {
         confReady === true &&
         (user.email === conference.ownerEmail || conference.confAdmins.includes(user.email)) &&
         <Container>
-          <Row>
-            <Col sm={2}>
-              {(formType === "edit_presenter" || formType === "admin_edit_pres")
-                ? <Button data-toggle="popover" title="Update" className="button" onClick={handleFormUpdate} type="submit">Update Form</Button>
-                : <Button data-toggle="popover" title="Submit" className="button" onClick={handleFormSubmit} type="submit">Submit Form</Button>}
-            </Col>
-          </Row>
+          <Form className="schedForm">
+
+            <Row>
+              <Col sm={2}>
+                {(formType === "edit_presenter" || formType === "admin_edit_pres")
+                  ? <Button data-toggle="popover" title="Update" className="button" onClick={handleFormUpdate} type="submit">Update Form</Button>
+                  : <Button data-toggle="popover" title="Submit" className="button" onClick={handleFormSubmit} type="submit">Submit Form</Button>}
+              </Col>
+            </Row>
+
+            <Card className="formCard">
+              <Card.Body className="cardBody">
+                <Row>
+                  <Col sm={12}>
+                    <Form.Group controlId="formSchedRooms">
+                      <Form.Label>Room names:</Form.Label><br />
+                      <Form.Text className="subtitle" muted>Please separate room names with commas.</Form.Text>
+                      <Form.Control type="input" name="schedRooms" placeholder="Enter room names here" value={schedule.schedRooms} className="formInput" onChange={handleInputChange} />
+                    </Form.Group>
+                  </Col>
+                </Row>
+
+                <Row>
+                  <Col sm={12}>
+                    <Form.Group controlId="formSchedTimes">
+                      <Form.Label>Time blocks:</Form.Label><br />
+                      <Form.Text className="subtitle" muted>Please use the form hh:mm am/pm-hh:mm am/pm and separate by commas.</Form.Text>
+                      <Form.Control type="input" name="schedTimes" placeholder="Enter time blocks here" value={schedule.schedTimes} className="formInput" onChange={handleInputChange} />
+                    </Form.Group>
+                  </Col>
+                </Row>
+              </Card.Body>
+            </Card>
+
+          </Form>
         </Container>}
     </>
   )
