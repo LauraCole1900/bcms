fdb3c39e7d8b855329b610f8b4db6de0e96d2711
Laura Cole <lauracole1900@comcast.net>
2021-05-20 20:16:04 -0600

    updated imports for sessProposals and sessPropDetailsPage

--- client/src/components/pages/sessProposals.js
+++ client/src/components/pages/sessProposals.js
@@ -1,4 +1,8 @@
 import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import { useAuth0 } from "@auth0/auth0-react";
+import { SessionAPI } from "../../utils/api";
+import "./style.css";
 
 const AllSessProps = () => {
 
