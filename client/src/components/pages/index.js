e99f144cc217241d21abe8f3a005c75270a71896
Laura Cole <lauracole1900@comcast.net>
2021-05-21 19:35:22 -0600

    updated allSessPropsPage import on pages/index

--- client/src/components/pages/index.js
+++ client/src/components/pages/index.js
@@ -1,6 +1,6 @@
 export { default as AboutBCMS } from "./aboutPage";
 export { default as AllConfs } from "./allConfsPage";
-export { default as AllSessProps } from "./sessProposals";
+export { default as AllSessProps } from "./allSessPropsPage";
 export { default as ConfDetails } from "./confDetailsPage";
 export { default as ConfExhibits } from "./confExhibitors";
 export { default as Schedule } from "./confSchedule";
