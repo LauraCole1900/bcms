b5b01d08b513158c5597158565f9db1b18858433
Laura Cole <lauracole1900@comcast.net>
2021-06-14 01:38:40 -0600

    adjusted spacing on schedSessCard, working on sessModal

--- client/src/components/cards/schedSessCard.js
+++ client/src/components/cards/schedSessCard.js
@@ -1,12 +1,26 @@
-import React from "react";
-import { Button, Card, Row } from "react-bootstrap";
+import React, { useState } from "react";
+import { Button, Card } from "react-bootstrap";
 import { useAuth0 } from "@auth0/auth0-react";
+import { SessionModal } from "../modals";
 import "./style.css";
 
 const SchedSessCard = (props) => {
-  console.log(props);
   const { user, isAuthenticated } = useAuth0();
   let nameArr = [];
+
+  // Modal variables
+  const [showSession, setShowSession] = useState(0);
+  const [thisId, setThisId] = useState();
+
+  const handleShowSession = (e) => {
+    const { dataset } = e.target;
+    console.log(dataset.sessid);
+    setThisId(dataset.sessid);
+  }
+  const handleHideSession = () => {
+    setShowSession(0);
+    props.change();
+  }
 
   // Filters props.presenter by sessId, then maps through the result to pull out presenter names
   const fetchPresNames = (sessId) => {
@@ -24,15 +38,9 @@ const SchedSessCard = (props) => {
         ? <>
           {props.session[0] !== undefined
             ? <Card className="sched">
-              <Row>
-                <h3 className="textTight">{props.session[0].sessName}</h3>
-              </Row>
-              <Row>
-                <p>{fetchPresNames(props.session[0]._id)}</p>
-              </Row>
-              <Row>
-                <Button className="button">Session Details</Button>
-              </Row>
+              <h3 className="textTight maxWidth">{props.session[0].sessName}</h3><br />
+              <p className="textTight maxWidth">{fetchPresNames(props.session[0]._id)}</p><br />
+              <Button className="button" data-sessid={props.session[0]._id} onClick={(e) => handleShowSession(e)}>Session Details</Button>
             </Card>
             : <Card className="schedBlue">
               <h3 className="textTight">Click to add session</h3>
@@ -41,20 +49,17 @@ const SchedSessCard = (props) => {
         : <>
           {props.session[0] !== undefined
             ? <Card className="sched">
-              <Row className="maxWidth">
-                <h3 className="textTight">{props.session[0].sessName}</h3><br />
-              </Row>
-              <Row className="maxWidth">
-                <p className="textTight">{fetchPresNames(props.session[0]._id)}</p><br />
-              </Row>
-              <Row className="maxWidth">
-                <Button className="button">Session Details</Button>
-              </Row>
+              <h3 className="textTight maxWidth">{props.session[0].sessName}</h3><br />
+              <p className="textTight maxWidth">{fetchPresNames(props.session[0]._id)}</p><br />
+              <Button className="button" data-sessid={props.session[0]._id} onClick={(e) => handleShowSession(e)}>Session Details</Button>
             </Card>
             : <Card className="schedBlue">
               <h3 className="textTight">FREE</h3>
             </Card>}
         </>}
+
+      {props.session[0] !== undefined &&
+        <SessionModal session={props.session[0]} presenter={props.presenters} conference={props.conference} show={showSession === props.session[0]._id} hide={(e) => handleHideSession(e)} />}
     </>
   )
 }
--- client/src/components/modals/index.js
+++ client/src/components/modals/index.js
@@ -1,3 +1,4 @@
 export { default as ConfirmModal } from "./confirm";
 export { default as ErrorModal } from "./error";
+export { default as SessionModal } from "./sessModal";
 export { default as SuccessModal } from "./success";
