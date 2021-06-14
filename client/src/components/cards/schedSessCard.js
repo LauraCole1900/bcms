8b28d83886e8273ed7481a36f21427d401790d85
Laura Cole <lauracole1900@comcast.net>
2021-06-14 14:37:49 -0600

    pseudocoded idea to handle adding session to schedule onClick

--- client/src/components/cards/schedSessCard.js
+++ client/src/components/cards/schedSessCard.js
@@ -9,15 +9,24 @@ const SchedSessCard = (props) => {
   let nameArr = [];
 
   // Modal variables
-  const [showSession, setShowSession] = useState(0);
+  const [showDetails, setShowDetails] = useState(0);
+  const [showAssign, setShowAssign] = useState(0);
 
-  const handleShowSession = (e) => {
+  const handleShowDetails = (e) => {
     const { dataset } = e.target;
     console.log(dataset.sessid);
-    setShowSession(dataset.sessid);
+    setShowDetails(dataset.sessid);
   }
-  const handleHideSession = () => {
-    setShowSession(0);
+  const handleHideDetails = () => {
+    setShowDetails(0);
+  }
+
+  const handleShowAssign = (e) => {
+    // Probably will make use of props.allSess, props.room, and props.time
+    // Bring up modal to choose from existing sessions + option to create new session
+    // New option sends user to session form
+    const { name, value, dataset } = e.target;
+
   }
 
   // Filters props.presenter by sessId, then maps through the result to pull out presenter names
@@ -26,12 +35,6 @@ const SchedSessCard = (props) => {
     const presName = thesePres.map(pres => pres.presGivenName + " " + pres.presFamilyName)
     nameArr = [presName.join(", ")]
     return nameArr;
-  }
-  
-  const sessAssign = (e) => {
-    // probably will make use of props.allSess, props.room, and props.time
-    const { name, value, dataset } = e.target;
-
   }
 
 
@@ -44,10 +47,10 @@ const SchedSessCard = (props) => {
             ? <Card className="sched">
               <h3 className="textTight maxWidth">{props.session[0].sessName}</h3><br />
               <p className="textTight maxWidth">{fetchPresNames(props.session[0]._id)}</p><br />
-              <Button className="button" data-sessid={props.session[0]._id} onClick={(e) => handleShowSession(e)}>Session Details</Button>
+              <Button className="button" data-sessid={props.session[0]._id} onClick={(e) => handleShowDetails(e)}>Session Details</Button>
             </Card>
             : <Card className="schedBlue">
-              <h3 className="textTight" onClick={sessAssign}>Click to add session</h3>
+              <h3 className="textTight" onClick={(e) => handleShowAssign(e)}>Click to add session</h3>
             </Card>}
         </>
         : <>
@@ -55,7 +58,7 @@ const SchedSessCard = (props) => {
             ? <Card className="sched">
               <h3 className="textTight maxWidth">{props.session[0].sessName}</h3><br />
               <p className="textTight maxWidth">{fetchPresNames(props.session[0]._id)}</p><br />
-              <Button data-toggle="popover" title="Session details" className="button" data-sessid={props.session[0]._id} onClick={(e) => handleShowSession(e)}>Session Details</Button>
+              <Button data-toggle="popover" title="Session details" className="button" data-sessid={props.session[0]._id} onClick={(e) => handleShowDetails(e)}>Session Details</Button>
             </Card>
             : <Card className="schedBlue">
               <h3 className="textTight">FREE</h3>
@@ -63,7 +66,7 @@ const SchedSessCard = (props) => {
         </>}
 
       {props.session[0] !== undefined &&
-        <SessionModal session={props.session[0]} presenter={props.presenters} conference={props.conference} show={showSession === props.session[0]._id} hide={(e) => handleHideSession(e)} />}
+        <SessionModal session={props.session[0]} presenter={props.presenters} conference={props.conference} show={showDetails === props.session[0]._id} hide={(e) => handleHideDetails(e)} />}
     </>
   )
 }
