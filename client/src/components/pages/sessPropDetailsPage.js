32e53fcd25925b63983c661228829b470cac0a68
Laura Cole <lauracole1900@comcast.net>
2021-06-23 21:36:41 -0600

    working on getting presenter names to render to sessPropDetailsPage, not working yet

--- client/src/components/pages/sessPropDetailsPage.js
+++ client/src/components/pages/sessPropDetailsPage.js
@@ -11,8 +11,10 @@ const SessPropDetails = () => {
   const { user, isAuthenticated, loginWithRedirect } = useAuth0();
   const [conference, setConference] = useState();
   const [session, setSession] = useState();
+  const [presArray, setPresArray] = useState();
   const [showSuccess, setShowSuccess] = useState(0);
   const [confReady, setConfReady] = useState(false);
+  const [presReady, setPresReady] = useState(false);
   const [sessReady, setSessReady] = useState(false);
 
   // Pull session ID from URL
@@ -35,8 +37,24 @@ const SessPropDetails = () => {
     setConfReady(true)
   }
 
+  const fetchPres = async (email, id) => {
+    let presObj;
+    return await PresenterAPI.getPresenterByEmail(email, id)
+      .then(resp => {
+        console.log("sessPropDetailsPage fetchPres", resp.data)
+        presObj = resp.data
+        console.log({ presObj })
+        return presObj;
+      })
+      .catch(err => {
+        console.log(err)
+        return false
+      })
+  }
+
   const fetchSess = async (id) => {
     let sessObj;
+    let presArr = [];
     await SessionAPI.getSessionById(id)
       .then(resp => {
         console.log("sessPropDetailsPage fetchSess", resp.data)
@@ -51,6 +69,21 @@ const SessPropDetails = () => {
       })
     // GET conference by session.confId
     fetchConf(sessObj[0].confId)
+    sessObj[0].sessPresEmails.forEach(email => {
+      fetchPres(email, sessObj[0].confId)
+      .then(resp => {
+        console.log(resp.data)
+        presArr = [...presArr, resp.data]
+        console.log({ presArr })
+        if (presArr.length === sessObj[0].sessPresEmails.length) {
+          setPresArray(presArr);
+          setPresReady(true);
+        }
+      })
+      .catch(err => {
+        console.log(err);
+      })
+    })
     setSessReady(true)
   }
 
@@ -74,6 +107,7 @@ const SessPropDetails = () => {
 
       {isAuthenticated &&
         sessReady === true &&
+        presReady === true &&
         confReady === true &&
         <Container>
 
@@ -87,6 +121,11 @@ const SessPropDetails = () => {
             <Col sm={10}>
               <Row>
                 <h1 className="center">Details for {session[0].sessName} Proposal</h1>
+                <h3>Presenters:
+                  {presArray.map(pres => (
+                    <p>{pres.presGivenName} {pres.presFamilyName}</p>
+                  ))}
+                </h3>
               </Row>
 
 
