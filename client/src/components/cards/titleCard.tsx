a6406a5c6e059483325740f77e2f05bf425ff77c
Laura Cole <lauracole1900@comcast.net>
2021-06-24 22:00:56 -0600

    typed TitleCard output

--- client/src/components/cards/titleCard.tsx
+++ client/src/components/cards/titleCard.tsx
@@ -1,8 +1,8 @@
-import React from "react";
+import React, { ReactElement } from "react";
 import { Container, Card } from "react-bootstrap";
 import "./style.css";
 
-const TitleCard = () => {
+const TitleCard = (): ReactElement => {
 
   return (
     <Container>
