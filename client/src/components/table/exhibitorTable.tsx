a9a2b1684e9e80d383c96deabc25e15b3aba4e51
Laura Cole <lauracole1900@comcast.net>
2021-06-27 21:36:09 -0600

    exhTable update booth num still doesn't work, not sure it's necessary

--- client/src/components/table/exhibitorTable.tsx
+++ client/src/components/table/exhibitorTable.tsx
@@ -1,4 +1,4 @@
-import React, { ChangeEvent, KeyboardEvent, ReactElement, useState } from "react";
+import React, { ChangeEvent, FormEvent, ReactElement, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { Location } from "history";
 import { Button, Form, Image } from "react-bootstrap";
@@ -26,34 +26,24 @@ interface Exhibitor {
 
 const ExhibitorTable = (props: any): ReactElement => {
   const location = useLocation<Location>();
-  const exhArr: Exhibitor[] = props.exhibitors
   const [exhibitor, setExhibitor] = useState<Exhibitor>();
-  let idx: number;
-
-  // finds the index of the document with the given ID
-  const findIndex = (e: ChangeEvent<HTMLInputElement>) => {
-    const { dataset } = e.target;
-    idx = exhArr.findIndex(exh => exh._id === dataset.id);
-    return idx;
-  };
 
   // handles input to booth number field
-  const handleInputChange = (e: ChangeEvent<HTMLInputElement>): string | void => {
-    const { name, value } = e.target;
-    findIndex(e);
-    setExhibitor({ ...exhArr[idx], [name]: value })
-  }
+  // const handleInputChange = (e: ChangeEvent<HTMLInputElement>): string | void => {
+  //   const { name, value } = e.target;
+  //   setExhibitor({ ...exhibitor!, [name]: value })
+  // }
 
   // API call to update exhibitor document onSubmit
-  const handleSubmit = (e: KeyboardEvent<HTMLElement>): string | void => {
-    const { dataset } = e.target as HTMLInputElement;
-    if (e.key === "Enter") {
-      console.log("from exhTable handleSubmit", { exhibitor })
-      ExhibitorAPI.updateExhibitor({ ...exhibitor }, dataset.id)
-        .then(props.exhcb(props.conference[0]._id))
-        .catch(err => console.log(err))
-    }
-  }
+  // const handleSubmit = (e: FormEvent<HTMLElement>): string | void => {
+  //   const { dataset } = e.target as HTMLInputElement;
+  //   if (e.key === "Enter") {
+  //     console.log("from exhTable handleSubmit", { exhibitor })
+  //     ExhibitorAPI.updateExhibitor({ ...exhibitor }, dataset.id)
+  //       .then(props.exhcb(props.conference[0]._id))
+  //       .catch(err => console.log(err))
+  //   }
+  // }
 
 
   return (
@@ -68,7 +58,8 @@ const ExhibitorTable = (props: any): ReactElement => {
           <td>{exh.exhWorkerNames}</td>
           <td>{exh.exhSpaces}</td>
           <td>{exh.exhAttend}</td>
-          <td><Form.Control type="input" name="exhBoothNum" value={exh.exhBoothNum} data-id={exh._id} className="formInput" onChange={handleInputChange} onKeyPress={handleSubmit} /></td>
+          <td>{exh.exhBoothNum}</td>
+          {/* <td><Form.Control type="input" name="exhBoothNum" value={exh.exhBoothNum} data-id={exh._id} className="formInput" onChange={handleInputChange} onSubmit={handleSubmit} /></td> */}
           <td>
             <Link to={`/admin_edit_exh/${exh._id}`} className={location.pathname === `/admin_edit_exh/${exh._id}` ? "link active" : "link"}>
               <Button data-toggle="popover" title="Edit this exhibit" className="tbleditbtn" name="attEdit">
